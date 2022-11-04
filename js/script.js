89a7fec87f93abf4c547ecf09465d97db978e916
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-11-04 17:44:23 -0300

    Tela inicial (Menu, Imagens, Texto e Botão)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Menu */
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+// Evento clique no menu
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Evento rolagem da tela que esconde o menu
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
