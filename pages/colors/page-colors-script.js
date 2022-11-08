e7b32fa6b7d68bb54e34381de20021703b2466e3
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-11-08 16:32:03 -0300

    Lógica para não repetir cores

--- pages/colors/page-colors-script.js
+++ pages/colors/page-colors-script.js
@@ -2,50 +2,85 @@
 
 const txtCor = document.querySelector('#cor');
 const arrayCoresStatic = [["AMARELO", "yellow"], 
-                    ["VERMELHO", "red"], 
-                    ["AZUL", "blue"], 
-                    ["VERDE", "green"], 
-                    ["ROSA", "pink"], 
-                    ["ROXO", "purple"], 
-                    ["LARANJA", "orange"]];
+                          ["VERMELHO", "red"], 
+                          ["AZUL", "blue"], 
+                          ["VERDE", "green"], 
+                          ["ROSA", "pink"], 
+                          ["ROXO", "purple"], 
+                          ["LARANJA", "orange"],
+                          ["MARROM", "#964b00"],
+                          ["CINZA", "gray"],
+                          ["VIOLETA", "violet"]
+                         ];
 
 const arrayBtnCor = document.querySelectorAll(".btn-cor");
 var btnCorCorreta = "";
+var arrayCoresRestantes = JSON.parse(JSON.stringify(arrayCoresStatic));
 
 function carregarCores(){
 
-    // Copiar valores do array de cores para um array auxiliar
-    var arrayCores = JSON.parse(JSON.stringify(arrayCoresStatic));
+    if (arrayCoresRestantes.length > 0) {
+        // Copiar valores do array de cores para um array auxiliar
+        var arrayCores = JSON.parse(JSON.stringify(arrayCoresStatic));
 
-    /* Math.random(): Número aleatório entre (0-1)
-    Math.random() * arr.length: Números entre (0-arrayLength)
-    Math.floor(): Arredondar o resultado do número aleatorio */
-    var indice = Math.floor(Math.random() * arrayCores.length);
-    txtCor.innerHTML = arrayCores[indice][0];
+        /* Math.random(): Número aleatório entre (0-1)
+        Math.random() * arr.length: Números entre (0-arrayLength)
+        Math.floor(): Arredondar o resultado do número aleatorio */
+        var indice = Math.floor(Math.random() * arrayCoresRestantes.length);
+        var cor = arrayCoresRestantes[indice][0];
+        txtCor.innerHTML = cor;
 
-    /* Carregar cores nos botões */
+        /* Carregar cores nos botões */
 
-    // Indice aleatorio para o botão com a cor correta
-    var indiceBtn = Math.floor(Math.random() * arrayBtnCor.length);
+        // Indice aleatorio para o botão com a cor correta
+        var indiceBtn = Math.floor(Math.random() * arrayBtnCor.length);
 
-    // Colore o botão com a cor correta
-    arrayBtnCor[indiceBtn].style.backgroundColor = arrayCores[indice][1];
-    btnCorCorreta = arrayBtnCor[indiceBtn];
+        // Colore o botão com a cor correta
+        arrayBtnCor[indiceBtn].style.backgroundColor = arrayCoresRestantes[indice][1];
+        btnCorCorreta = arrayBtnCor[indiceBtn];
 
-    // Pinta os outros botões
-    for (let i = 0; i < arrayBtnCor.length; i++) {
-        if (i != indiceBtn) {
-            // Remove a cor já usada do array das cores
-            arrayCores.splice(indice, 1);
-            indice = Math.floor(Math.random() * arrayCores.length);
-            
-            arrayBtnCor[i].style.backgroundColor = arrayCores[indice][1];  
+        // Remove a cor do array das cores
+        arrayCores.splice(indiceCorArray(cor, arrayCores), 1);
+        //console.log("arrayCores.indexOf(cor) " , indiceCorArray(cor, arrayCores));
+
+        // Pinta os outros botões
+        for (let i = 0; i < arrayBtnCor.length; i++) {
+            if (i != indiceBtn) {
+                
+                //console.log("arrayCores: " , arrayCores)
+                indice = Math.floor(Math.random() * arrayCores.length);
+                
+                arrayBtnCor[i].style.backgroundColor = arrayCores[indice][1];  
+
+                // Remove a cor já usada do array das cores
+                arrayCores.splice(indice, 1);
+            } 
         }
-        
+
+        arrayCoresRestantes.splice(indiceCorArray(cor, arrayCoresRestantes), 1);
+
+        //console.log("arrayCoresRestantes: " , arrayCoresRestantes)
+    } else {
+        arrayCoresRestantes = JSON.parse(JSON.stringify(arrayCoresStatic));
+
+        modalText.innerHTML = "Parabéns, você acertou todas as cores!";
+        modalButton.innerHTML = "Continuar";
+        modal.style.display = 'block';
     }
 }
 
 carregarCores();
+
+/* Retornar indice de uma cor de um array */
+
+function indiceCorArray(color, array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][0] == color) {
+            return i;
+        }
+    }
+    return "";
+}
 
 /* Evento clique nos botões das cores */
 
